ccec9f009cb658f60fcb7566c98ce2f1eb32d447
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2022-01-03 18:31:14 +0200

    Initial implementation of menu modal

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function onClick() {
+    var modal = document.getElementById("menuModal");
+    var btn = document.getElementById("menuLink");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}

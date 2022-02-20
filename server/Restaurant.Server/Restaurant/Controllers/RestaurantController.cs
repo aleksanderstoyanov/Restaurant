3d00ec77158efee5608ebd740d4d39af84fa60ba
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2022-02-20 12:52:27 +0200

    Add initial server layer commit

---
+++ server/Restaurant.Server/Restaurant/Controllers/RestaurantController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantController : Controller
+    {
+       [HttpGet]
+       public IActionResult Get()
+        {
+            return Ok("Hello: test,test");
+        }
+    }
+}
